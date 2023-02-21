a368c892c07f72f9fe948820acdbea24f2fa1571
[GAL1LE0] <olilebel2004@hotmail.com>
2023-02-21 11:22:21 -0500

    changements

---
+++ LunarLander/Assets/Vaisseau.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Vaisseau : MonoBehaviour
+{
+    private SpriteRenderer rend;
+    public Sprite VAF, VSF;
+    public Text altitude;
+
+    void Start()
+    {
+        rend = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        float alt = (gameObject.transform.position.y + 5) * 10;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            vaisseauAvecFeu();
+            
+
+        }
+        else if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            vaisseauSansFeu();
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if(gameObject.transform.rotation.z != 90)
+            {
+                transform.Rotate(new Vector3(0, 0, (float)0.2));
+            }
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if (gameObject.transform.rotation.z != -90)
+            {
+                transform.Rotate(new Vector3(0, 0, (float)-0.2));
+            }
+        }
+
+        altitude.text = alt.ToString();
+    }
+
+    void vaisseauAvecFeu()
+    {
+        rend.sprite = VAF;
+    }
+    void vaisseauSansFeu()
+    {
+        rend.sprite = VSF;
+    }
+}
