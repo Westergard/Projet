d7405035377706842aef65ef79f9fb0b2ba64398
[GAL1LE0] <olilebel2004@hotmail.com>
2023-03-01 10:52:08 -0500

    Changements

--- LunarLander/Assets/Vaisseau.cs
+++ LunarLander/Assets/Vaisseau.cs
@@ -9,6 +9,8 @@ public class Vaisseau : MonoBehaviour
     private SpriteRenderer rend;
     public Sprite VAF, VSF;
     public Text altitude;
+    public Text XVelocity;
+    public Text YVelocity;
 
     void Start()
     {
@@ -18,15 +20,32 @@ public class Vaisseau : MonoBehaviour
     void Update()
     {
         float alt = (gameObject.transform.position.y + 5) * 10;
+        Vector2 velocity = myRigidBody.velocity;
 
         float pushX;
         float pushY;
         if (Input.GetKey(KeyCode.UpArrow))
         {
             vaisseauAvecFeu();
-            pushX = 1 * Mathf.Sin(myRigidBody.transform.rotation.z);
-            pushY = 1 * Mathf.Cos(myRigidBody.transform.rotation.z);
-            myRigidBody.velocity = new Vector3(pushX, pushY, 0);
+
+            if (myRigidBody.transform.rotation.z < 0.72 && myRigidBody.transform.rotation.z > 0)
+            {
+                pushX = 1 * Mathf.Sin(myRigidBody.transform.rotation.z);
+                pushY = 1 * Mathf.Cos(myRigidBody.transform.rotation.z);
+
+                myRigidBody.velocity = new Vector2(-pushX, pushY);
+            }
+            if (myRigidBody.transform.rotation.z > -0.72 && myRigidBody.transform.rotation.z < 0)
+            {
+                pushX = 1 * Mathf.Sin(myRigidBody.transform.rotation.z);
+                pushY = 1 * Mathf.Cos(myRigidBody.transform.rotation.z);
+
+                myRigidBody.velocity = new Vector2(-pushX, pushY);
+            }
+            if(myRigidBody.transform.rotation.z <0.01 && myRigidBody.transform.rotation.z > -0.01)
+            {
+                myRigidBody.velocity = new Vector2(0, 1);
+            }
 
         }
         else if (Input.GetKeyUp(KeyCode.UpArrow))
@@ -36,20 +55,22 @@ public class Vaisseau : MonoBehaviour
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            if(myRigidBody.transform.rotation.z < 90)
+            if(myRigidBody.transform.rotation.z < 0.71)
             {
                myRigidBody.transform.Rotate(new Vector3(0, 0, (float)0.2));   
             }
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            if (myRigidBody.transform.rotation.z > -90)
+            if (myRigidBody.transform.rotation.z > -0.71)
             {
                 myRigidBody.transform.Rotate(new Vector3(0, 0, (float)-0.2));
             }
         }
 
         altitude.text = alt.ToString();
+        XVelocity.text = velocity.x.ToString();
+        YVelocity.text = velocity.y.ToString();
     }
 
     void vaisseauAvecFeu()
