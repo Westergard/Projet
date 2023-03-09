94df8e442f77a3bf557d1c6e869d4efbb186e79d
[GAL1LE0] <olilebel2004@hotmail.com>
2023-03-09 16:35:22 -0500

    Changements

--- LunarLander/Assets/SCRIPTS/Jeu/Tourelle.cs
+++ LunarLander/Assets/SCRIPTS/Jeu/Tourelle.cs
@@ -6,12 +6,14 @@ public class Tourelle : MonoBehaviour
 {
     public GameObject Vaisseau;
     public GameObject Laser;
+    Rigidbody2D myRigidBody;
     Animator m_Animator;
     GameObject newLaser;
 
     void Start()
     {
         m_Animator = gameObject.GetComponent<Animator>();
+        myRigidBody = gameObject.GetComponent<Rigidbody2D>();
     }
 
     void Update()
@@ -19,18 +21,23 @@ public class Tourelle : MonoBehaviour
         
         if (Vaisseau != null)
         {
-            if (newLaser == null)
+            Vector2 positionVaisseau = Vaisseau.transform.position;
+            if (Mathf.Sqrt(Mathf.Pow(positionVaisseau.x - myRigidBody.position.x, 2) + Mathf.Pow(positionVaisseau.y - myRigidBody.position.y, 2)) <= 5)
             {
-                newLaser = Instantiate(Laser);
+                if (newLaser == null)
+                {
+                    newLaser = Instantiate(Laser);
+                }
+                if (Vaisseau.transform.position.x > gameObject.transform.position.x)
+                {
+                    m_Animator.SetTrigger("Droite");
+                }
+                else if (Vaisseau.transform.position.x < gameObject.transform.position.x)
+                {
+                    m_Animator.SetTrigger("Gauche");
+                }
             }
-            if (Vaisseau.transform.position.x > gameObject.transform.position.x)
-            {
-                m_Animator.SetTrigger("Droite");
-            }
-            else if (Vaisseau.transform.position.x < gameObject.transform.position.x)
-            {
-                m_Animator.SetTrigger("Gauche");
-            }
+            
         }
     }
 }
