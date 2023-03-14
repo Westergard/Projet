22814124cd37642483abf48ae17e2cf2e21904fe
JoeBombard <Joseph@jophem.com>
2023-03-14 11:33:26 -0400

    changement

--- LunarLander/Assets/curve/PathEditor.cs
+++ LunarLander/Assets/curve/PathEditor.cs
@@ -8,6 +8,8 @@ public class PathEditor : Editor {
 
     PathCreator creator;
     Path path;
+    LineRenderer lineRenderer;
+    int SEGMENT_COUNT = 50;
 
     void OnSceneGUI()
     {
@@ -39,7 +41,7 @@ public class PathEditor : Editor {
             Handles.DrawBezier(points[0], points[3], points[1], points[2], Color.green, null, 2);
         }
 
-        /**/Handles.color = Color.red;
+        /*Handles.color = Color.red;
         for (int i = 0; i < path.NumPoints; i++)
         {
             Vector2 newPos = Handles.FreeMoveHandle(path[i], Quaternion.identity, .1f, Vector2.zero, Handles.CylinderHandleCap);
@@ -48,7 +50,46 @@ public class PathEditor : Editor {
                 Undo.RecordObject(creator, "Move point");
                 path.MovePoint(i, newPos);
             }
+        }*/
+    }
+    /*void DrawCurve()
+    {
+        for (int j = 0; j < path.NumSegments; j++)
+        {
+            Vector2[] points = path.GetPointsInSegment(j);
+            Vector2 temp;  
+            for (int i = 1; i <= SEGMENT_COUNT; i++)
+            {
+                float t = i / (float)SEGMENT_COUNT;
+                int nodeIndex = j * 3;
+                Vector2 pixel = CalculateCubicBezierPoint(t, points[0], points[1], points[2], points[3]);
+                
+                if (i>2) {
+                    Handles.color = Color.green;
+                    Handles.DrawLine(pixel, temp);
+                }
+                temp = pixel;
+                lineRenderer.SetVertexCount(((j * SEGMENT_COUNT) + i));
+                lineRenderer.SetPosition((j * SEGMENT_COUNT) + (i - 1), pixel);
+            }
         }
+
+        
+    }*/
+    Vector2 CalculateCubicBezierPoint(float t, Vector2 p0, Vector2 p1, Vector2 p2, Vector2 p3)
+    {
+        float u = 1 - t;
+        float tt = t * t;
+        float uu = u * u;
+        float uuu = uu * u;
+        float ttt = tt * t;
+        
+        Vector2 p = uuu * p0; 
+        p += 3 * uu * t * p1; 
+        p += 3 * u * tt * p2; 
+        p += ttt * p3; 
+        
+        return p;
     }
 
     void OnEnable()
