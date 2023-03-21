7e9db6b9cd18a6b8e0964ed8adce4221fe456b13
[GAL1LE0] <olilebel2004@hotmail.com>
2023-03-21 10:42:52 -0400

    Musique

--- LunarLander/Assets/SCRIPTS/Settings/DontDestroyAudio.cs
+++ LunarLander/Assets/SCRIPTS/Settings/DontDestroyAudio.cs
@@ -1,11 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DontDestroyAudio : MonoBehaviour
 {
+    public static DontDestroyAudio instance;
+    public AudioSource audio;
+
+    void Update()
+    {
+        string currentScene = SceneManager.GetActiveScene().name;
+        if (currentScene == "Jeu Perlin")
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+    }
     void Awake()
     {
-        DontDestroyOnLoad(transform.gameObject);
+        string currentScene = SceneManager.GetActiveScene().name;
+        if (instance == null && currentScene != "Jeu Perlin")
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        
+            DontDestroyOnLoad(this.gameObject);
     }
 }
