5cb7d826bacff52209dab41d1725094759c31c3f
MickyTheKing <miclecool4@gmail.com>
2023-04-25 10:16:27 -0400

    Change

--- LunarLander/Assets/SCRIPTS/Settings/ChangementScene.cs
+++ LunarLander/Assets/SCRIPTS/Settings/ChangementScene.cs
@@ -10,13 +10,18 @@ public class ChangementScene : MonoBehaviour
 {
     public TMPro.TMP_Dropdown m_Dropdown;
     public TMPro.TMP_Dropdown m_level;
+    public TMPro.TMP_Dropdown m_levelSecret;
     public Slider m_SliderVolumePincipale;
     public Slider m_SliderEffetSonore;
     public Slider m_SliderMusique;
     public Toggle t;
 
+    TMPro.TMP_Dropdown m_levelSecret1;
+
     void Start()
     {
+        m_levelSecret1 = Instanciate(m_levelSecret);
+        m_levelSecret.gameObject.SetActive(false);
     }
 
     void Update()
@@ -54,18 +59,19 @@ public class ChangementScene : MonoBehaviour
         PlayerPrefs.SetFloat("Musique", m_SliderMusique.value);
         PlayerPrefs.SetInt("Level", m_level.value+1);
 
-        if(t.isOn)
+       /* if(t.isOn)
         {
             PlayerPrefs.SetInt("high score", 0);
             PlayerPrefs.SetFloat("high time", 0);
-        }
+        } */
 
         SceneManager.LoadScene("MainMenu");
     }
 
     public void ClickSecretButton()
     {
-        
+        m_level.gameObject.SetActive(false);
+        m_levelSecret.gameObject.SetActive(true);
     }
 
     public void QuitGame()
