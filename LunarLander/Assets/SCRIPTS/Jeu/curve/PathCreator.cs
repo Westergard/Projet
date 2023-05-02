f4ece1ff72e411f17e9609868869cd425a0791eb
JoeBombard <Joseph@jophem.com>
2023-05-02 10:21:31 -0400

    exemple de curve

---
+++ LunarLander/Assets/SCRIPTS/Jeu/curve/PathCreator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathCreator : MonoBehaviour {
+
+    [HideInInspector]
+    public PathExemeple path;
+
+    public void CreatePath()
+    {
+        path = new PathExemeple(transform.position);
+    }
+}
